4d3372954ed75f04d540b060b3a01b1e43f259f0
kiraboannet624 <kiraboannet624@gmail.com>
2026-03-08 22:51:31 +0200

    added app jsx

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,8 +1,41 @@
+import { useState, useEffect } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+import { fetchEmployees } from "./services/api";
+
 function App() {
+  const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getEmployees = async () => {
+      try {
+        const data = await fetchEmployees();
+        setEmployees(data);
+      } 
+      catch  {
+        setError("Error fetching employees");
+      } finally {
+        setLoading(false);
+      }
+    
+    };
+  
+    getEmployees();
+  }, []);
+
+  if (loading) return <p>Loading employees...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
     <div>
-      <h1 className="text-center font-bold">kirabo's first react assignment</h1>
+      <h1>Employee Directory</h1>
+
+      {employees.map((employee) => (
+        <EmployeeCard key={employee.id} employee={employee} />
+      ))}
     </div>
   );
 }
+
 export default App;
