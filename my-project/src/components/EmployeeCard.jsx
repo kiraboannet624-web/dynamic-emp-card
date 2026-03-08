a40e29de7c259a379833a7ab842dcfa69daed862
kiraboannet624 <kiraboannet624@gmail.com>
2026-03-08 22:40:36 +0200

    added empcard

---
+++ my-project/src/components/EmployeeCard.jsx
@@ -0,0 +1,16 @@
+function EmployeeCard({ employee }) {
+  const { id, name, email, phone, website, company } = employee;
+
+  return (
+    <div className="card">
+      <h3>{name}</h3>
+      <p><strong>ID:</strong> {id}</p>
+      <p><strong>Email:</strong> {email}</p>
+      <p><strong>Phone:</strong> {phone}</p>
+      <p><strong>Website:</strong> {website}</p>
+      <p><strong>Company:</strong> {company.name}</p>
+    </div>
+  );
+}
+
+export default EmployeeCard;
